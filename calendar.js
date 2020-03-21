5ad3d61123d0ad65175494447b84518d4f6a0db9
Abhay Bhat <abhaybhat784@gmail.com>
2020-03-21 22:06:46 +0530

    UI updatesd

--- calendar.js
+++ calendar.js
@@ -1,6 +1,5 @@
 var month = 1;
 var year = 0;
-var month_name = ["NONE","JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"]
 function isLeapYear(year){
 	if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
 		return 1;
@@ -33,62 +32,53 @@ function daysOfMonth(month){
 
 function getCalendar(month, year){
 	let today = new Date();
+	let day = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+	let month_name = ["NONE","JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
+	let head = `<div class = "none"></div>
+                <button class = "card btn pn" onclick="getPrev()">❮ Prev</button>
+                <select class = "card slt" id = "months"></select>
+                <input type="number" class = "card ipt" id = "year">
+                <button class = "card btn" id="go" onclick="Go()">Go</button>
+                <button class = "card btn pn" onclick="getNext()">Next ❯</button>
+                <div class="none"></div>`;
+	let grid = "<div class='card'>";
+	let day_grid = "<div class='card head'>"
+	let empty_grid = "<div class='none'></div>";
+	let grid_today = "<div class='card today'>";
+	let grid_sun = "<div class = 'card sun'>";
+	let end = "</div>";
+	let main = $('#main');
+	let first_day = firstDayOfYear(year);
+	let leap_year = isLeapYear(year);
+	let number_of_days = daysOfMonth(month);
 
-	let day = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
-	let row_text_begin = "<div class='row'>"
-	let head_text_begin = "<div class='card head'>"
-	let grid_text_begin = "<div class='card'>"
-	let non_grid_text_begin = "<div class='non'>"
-	let grid_today_begin = "<div class='card today'>"
-	end = "</div>"
+	console.log(month);
 
-	let calendar_grid = Array(7)
-	calendar_grid.fill(row_text_begin)
-
-	let first_day = firstDayOfYear(year)
-	let leap_year = isLeapYear(year)
-	let number_of_days = daysOfMonth(month)
-
+	main.empty();
+	main.append(head);
+	for(let i = 1; i < 13; i++)
+		$('#months').append("<option>"+month_name[i]+"</option>");
+	for(let i = 0; i < 7; i++)
+		main.append(day_grid+day[i]+end);
 	document.getElementById('months').options[month-1].selected = true;
 	$('#year').val(year);
 
-	//$('#mm-yy').text(month_name[month]+' '+year)
-	for(let i = 0; i < 7; i++)
-		calendar_grid[0] += head_text_begin+day[i]+end;
-	
-	for(let i=1; i < month; i++)
-		first_day += daysOfMonth(i)
+	for(let i = 1; i < month; i++)
+		first_day += daysOfMonth(i);
 	if(month > 2)
-		first_day += leap_year
+		first_day += leap_year;
 	if(month == 2)
-		number_of_days += leap_year 
-	first_day %= 7
-	
-	j = 0
-	next = 1
-	for(let i = 0; i < first_day; i++){
-		calendar_grid[next] += non_grid_text_begin+end;
-		j += 1
-	}
-	
+		number_of_days += leap_year;
+	first_day %= 7;
+
+	for(let i = 0; i < first_day; i++)
+		main.append(empty_grid)
 	for(let i = 0; i < number_of_days; i++){
-		if(j % 7 == 0)
-			next += 1
-		if(year == today.getFullYear() && month == today.getMonth()+1 && (i+1) == today.getDate())
-			calendar_grid[next] += grid_today_begin+(i+1)+end;
+		if((i+1) == today.getDate() && month == today.getMonth()+1 && year == today.getFullYear())
+			main.append(grid_today+(i+1)+end)
 		else
-			calendar_grid[next] += grid_text_begin+(i+1)+end;
-		j += 1
+			main.append(grid+(i+1)+end)
 	}
-	
-	while(j % 7 != 0){
-		calendar_grid[next] += non_grid_text_begin+end;
-		j += 1
-	}
-	
-	$('#calendar').empty()
-	for(let i = 0; i < 7; i++)
-		$('#calendar').append(calendar_grid[i]+end);
 }
 
 function getNext(){
@@ -101,14 +91,10 @@ function getNext(){
 }
 
 function getPrev(){
-	if(year == 0 && month == 1)
-		return;
 	month -= 1;
-	if(month < 0){
-		if(year != 0){
-			year -= 1;
-			month = 12;
-		}
+	if(month < 1){
+		year -= 1;
+		month = 12;
 	}
 	getCalendar(month, year);
 }
@@ -120,8 +106,6 @@ function Go(){
 }
 
 function main(){
-	for(let i = 1; i < 13; i++)
-		$('#months').append("<option>"+month_name[i]+"</option>");
 	date = new Date()
 	year = date.getFullYear();
 	month = date.getMonth() + 1;
